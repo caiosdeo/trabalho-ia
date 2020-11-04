19580eefaa26dbecc147d1fe247ed79507cba800
Elias Cyrino <eliascyrino@gmail.com>
2020-11-04 17:56:48 -0300

    initializer to rule variable added

--- classes/table.cpp
+++ classes/table.cpp
@@ -9,6 +9,7 @@ Table::Table(unsigned int size){
     this->father = nullptr;
     this->size = size;
     this->indexOfVoidSpace = size / 2;
+    this->rule = 0;
     this->tokens = new unsigned int(size);
     this->cost = 0;
 
